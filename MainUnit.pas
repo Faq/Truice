b00025f4fcdd1cac57a3c7fa5d663fa44b603ca9
faq <ainarsh@gmail.com>
2013-08-13 02:37:55 +0300

    Release of Truice v1.3.7

--- MainUnit.pas
+++ MainUnit.pas
@@ -15,7 +15,7 @@ uses
 const
   VERSION_1   = '1'; //*10000
   VERSION_2   = '3'; //*100
-  VERSION_3   = '6';
+  VERSION_3   = '7';
   VERSION_EXE = VERSION_1 + '.' + VERSION_2 + '.' + VERSION_3;
 
   SCRIPT_TAB_NO_QUEST       = 8;
